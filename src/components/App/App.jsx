64e608fda3f3f6d886c8ffcdae785d31f126538d
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-02-24 16:05:11 -0600

    Link tag

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+// import CustomerForm from '../CustomerForm/CustomerForm';
 
 
 function App() {
@@ -24,14 +26,15 @@ function App() {
 	} // end of getPizza
 
 	return (
+		
 		<div className='App'>
 			<header className='App-header'>
 				<h1 className='App-title'>Prime Pizza</h1>
 			</header>
-
 			<img src='images/pizza_photo.png' />
 			<p>Pizza is great.</p>
 		</div>
+		
 	);
 }
 
