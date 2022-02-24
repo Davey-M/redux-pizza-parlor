9e7f57390bb4c990923a775c7ff9889429a15882
Davey-M <davey.meuer@gmail.com>
2022-02-24 11:45:44 -0600

    add axios

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -1,8 +1,19 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 function AdminView() {
 	const [orders, setOrders] = useState([]);
 
+	axios
+		.get('/orders')
+		.then((response) => {
+			setOrders(response.data);
+		})
+		.catch((err) => {
+			console.error('Error getting orders', err);
+		});
+
+	console.log(orders);
 	return (
 		<div className='adminView'>
 			<h1>Pizza Orders</h1>
