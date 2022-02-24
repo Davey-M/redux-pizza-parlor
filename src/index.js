ad37ebb5f68ad4c20332d05e7a2c7ae50e352573
vincesaykao <vtsaykao@hotmail.com>
2022-02-24 11:03:33 -0600

    updated GET with dispatch,axios and index reducer with imports

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+// Pizza reducer for GET from App.jsx
+const allPizzasReducer = (state = [], action) => {
+    if (action.type === 'GET_PIZZAS') {
+        return action.payload // returns only what is currently updated
+    }
+    return state
+}; // end of allPizzasReducer
+
+const storeInstance = createStore(
+    combineReducers({
+        allPizzasReducer,
+    }),
+    applyMiddleware(logger),
+
+); // end of storeInstance
+
+
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
