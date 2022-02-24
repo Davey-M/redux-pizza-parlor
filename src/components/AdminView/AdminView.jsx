b07b4cdac5b00bc98f08441557a78e899686b55e
Davey-M <davey.meuer@gmail.com>
2022-02-24 11:41:52 -0600

    add table

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -1,3 +1,24 @@
-function AdminView() {}
+import { useState } from 'react';
+
+function AdminView() {
+	const [orders, setOrders] = useState([]);
+
+	return (
+		<div className='adminView'>
+			<h1>Pizza Orders</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Time Order Placed</th>
+						<th>Type</th>
+						<th>Cost</th>
+					</tr>
+				</thead>
+				<tbody></tbody>
+			</table>
+		</div>
+	);
+}
 
 export default AdminView;
