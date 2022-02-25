d3bbaec6311952f6af6435aeaca23c4935ec4f2d
Davey-M <davey.meuer@gmail.com>
2022-02-25 12:49:20 -0600

    change text color

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { HashRouter as Router, useHistory } from 'react-router-dom';
 
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Paper } from '@mui/material';
 
 function CustomerForm() {
 	const history = useHistory();
@@ -51,93 +51,95 @@ function CustomerForm() {
 			<Router>
 				<h1>Add Info</h1>
 				<section>
-					<form
-						onSubmit={handleSubmit}
-						className='add-info-form'
-						style={{
-							backgroundColor: 'white',
-							width: '60%',
-							padding: '1rem',
-							borderRadius: '4px',
-							margin: 'auto',
-						}}
-					>
-						<TextField
+					<Paper>
+						<form
+							onSubmit={handleSubmit}
+							className='add-info-form'
 							style={{
-								minWidth: '40%',
+								backgroundColor: 'white',
+								width: '60%',
+								padding: '1rem',
+								borderRadius: '4px',
+								margin: 'auto',
 							}}
-							required
-							label='Name'
-							value={name}
-							onChange={(event) => setName(event.target.value)}
-						/>
-
-						<div className='break'></div>
-
-						<TextField
-							style={{
-								minWidth: '40%',
-							}}
-							required
-							label='Address'
-							value={address}
-							onChange={(event) => setAddress(event.target.value)}
-						/>
-
-						<div className='break'></div>
-
-						<TextField
-							style={{
-								minWidth: 'calc(20% - .5rem)',
-							}}
-							required
-							label='City'
-							value={city}
-							onChange={(event) => setCity(event.target.value)}
-						/>
-
-						<div className='widthBreak'></div>
-
-						<TextField
-							style={{
-								minWidth: 'calc(20% - .5rem)',
-							}}
-							required
-							label='Zip'
-							value={zip}
-							onChange={(event) => setZip(event.target.value)}
-						/>
-
-						<div className='break'></div>
-
-						<label>
-							<input
-								type='radio'
-								value='Pickup'
-								onChange={handleRadio}
-								checked={type === 'Pickup'}
+						>
+							<TextField
+								style={{
+									minWidth: '40%',
+								}}
+								required
+								label='Name'
+								value={name}
+								onChange={(event) => setName(event.target.value)}
 							/>
-							Pickup
-						</label>
-						<label>
-							<input
-								type='radio'
-								value='Delivery'
-								onChange={handleRadio}
-								checked={type === 'Delivery'}
+
+							<div className='break'></div>
+
+							<TextField
+								style={{
+									minWidth: '40%',
+								}}
+								required
+								label='Address'
+								value={address}
+								onChange={(event) => setAddress(event.target.value)}
 							/>
-							Delivery
-						</label>
-						{/* <Route path="/checkout" exact> */}
 
-						{/* <button type='submit'>Submit Info</button> */}
+							<div className='break'></div>
 
-						<div className='break'></div>
+							<TextField
+								style={{
+									minWidth: 'calc(20% - .5rem)',
+								}}
+								required
+								label='City'
+								value={city}
+								onChange={(event) => setCity(event.target.value)}
+							/>
 
-						<Button variant='contained' type='submit'>
-							Next
-						</Button>
-					</form>
+							<div className='widthBreak'></div>
+
+							<TextField
+								style={{
+									minWidth: 'calc(20% - .5rem)',
+								}}
+								required
+								label='Zip'
+								value={zip}
+								onChange={(event) => setZip(event.target.value)}
+							/>
+
+							<div className='break'></div>
+
+							<label>
+								<input
+									type='radio'
+									value='Pickup'
+									onChange={handleRadio}
+									checked={type === 'Pickup'}
+								/>
+								Pickup
+							</label>
+							<label>
+								<input
+									type='radio'
+									value='Delivery'
+									onChange={handleRadio}
+									checked={type === 'Delivery'}
+								/>
+								Delivery
+							</label>
+							{/* <Route path="/checkout" exact> */}
+
+							{/* <button type='submit'>Submit Info</button> */}
+
+							<div className='break'></div>
+
+							<Button variant='contained' type='submit'>
+								Next
+							</Button>
+						</form>
+					</Paper>
 				</section>
 			</Router>
 		</>
